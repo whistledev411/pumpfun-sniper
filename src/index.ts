c83891b957caf2f88096492beb8a98dc4b84e48e
whistledev411 <antonisloannou46@gmail.com>
2025-01-14 00:48:24 -0800

    add src

---
+++ src/index.ts
@@ -0,0 +1,222 @@
+import {
+    Connection,
+    Keypair,
+    PublicKey,
+} from "@solana/web3.js";
+import base58 from "bs58";
+import dotnet from 'dotenv'
+import { commitment, PUMP_FUN_PROGRAM } from "./constants";
+import { convertHttpToWebSocket, formatDate } from "./utils/commonFunc";
+
+import WebSocket = require("ws");
+import buyToken from "./pumputils/utils/buyToken";
+import { Metaplex } from "@metaplex-foundation/js";
+
+dotnet.config();
+
+
+const rpc = process.env.RPC_ENDPOINT;
+console.log("🚀 RPC:", rpc)
+
+const payer = process.env.PRIVATE_KEY;
+console.log("🚀 Private Key:", `${payer?.slice(0, 6)}...`)
+
+const isDevMode = process.env.DEV_MODE === 'true';
+const devwallet = process.env.DEV_WALLET_ADDRESS;
+if (isDevMode) {
+    console.log("🚀 Dev Wallet:", devwallet)
+}
+
+const isTickerMode = process.env.TICKER_MODE === 'true';
+const tokenTicker = process.env.TOKEN_TICKER;
+if (isTickerMode) {
+    console.log("🚀 Token Ticker:", tokenTicker)
+}
+
+const buyamount = process.env.BUY_AMOUNT;
+console.log("🚀 Buy Amount:", buyamount)
+
+const isGeyser = process.env.IS_GEYSER === 'true';
+
+const init = async (rpcEndPoint: string, payer: string, solIn: number, devAddr: string) => {
+    try {
+        const payerKeypair = Keypair.fromSecretKey(base58.decode(payer));
+        let isBuying = false;
+        const connection = new Connection(rpcEndPoint, { wsEndpoint: convertHttpToWebSocket(rpcEndPoint), commitment: "confirmed" });
+        const logConnection = new Connection(rpcEndPoint, { wsEndpoint: convertHttpToWebSocket(rpcEndPoint), commitment: "processed" });
+        let globalLogListener: any;
+
+        // Function to stop the listener
+        const stopListener = async () => {
+            if (globalLogListener !== undefined) {
+                try {
+                    await logConnection.removeOnLogsListener(globalLogListener);
+                    isBuying = true
+                } catch (err) {
+                    console.log("Error stopping listener:", err);
+                }
+            }
+        };
+
+        console.log('--------------- Bot is Runnig Now ---------------')
+
+        globalLogListener = logConnection.onLogs(
+            PUMP_FUN_PROGRAM,
+            async ({ logs, err, signature }) => {
+                if (err) return
+                const isMint = logs.filter(log => log.includes("MintTo")).length;
+                if (isMint && !isBuying) {
+                    const parsedTransaction = await logConnection.getParsedTransaction(signature, { maxSupportedTransactionVersion: 0, commitment: "confirmed" });
+                    if (!parsedTransaction) {
+                        return;
+                    }
+                    console.log("New signature => ", `https://solscan.io/tx/${signature}`, await formatDate());
+                    let dev = parsedTransaction?.transaction.message.accountKeys[0].pubkey.toString();
+                    const mint = parsedTransaction?.transaction.message.accountKeys[1].pubkey;
+                    if (isDevMode) {
+                        console.log("Dev wallet => ", `https://solscan.io/address/${dev}`);
+                    }
+                    if (isDevMode && dev !== devAddr) return;
+
+                    if (isTickerMode) {
+                        if (!tokenTicker) return console.log("Token Ticker is not defiend!");
+                        const tokenInfo = await getTokenMetadata(mint.toString(), connection);
+                        if (!tokenInfo) return;
+                        const isTarget = tokenInfo.symbol.toUpperCase().includes(tokenTicker.toUpperCase())
+                        if (!isTarget) return
+                        console.log(`Found $${tokenInfo.symbol} token.`)
+                    }
+
+                    console.log('New token => ', `https://solscan.io/token/${mint.toString()}`)
+                    await stopListener()
+                    isBuying = true;
+                    const sig = await buyToken(mint, connection, payerKeypair, solIn, 1);
+                    console.log('Buy Transaction => ', `https://solscan.io/tx/${sig}`)
+                    if (!sig) {
+                        isBuying = false;
+                    } else {
+                        console.log('🚀 Buy Success!!!');
+                        console.log('Try to sell on pumpfun: ', `https://pump.fun/${mint.toString()}`)
+                    }
+
+                }
+            },
+            commitment
+        );
+
+    } catch (err) {
+        console.log(err);
+        return { stopListener: undefined };
+    }
+};
+
+const withGaser = (rpcEndPoint: string, payer: string, solIn: number, devAddr: string) => {
+    const GEYSER_RPC = process.env.GEYSER_RPC;
+    if (!GEYSER_RPC) return console.log('Geyser RPC is not provided!');
+    const ws = new WebSocket(GEYSER_RPC);
+    const connection = new Connection(rpcEndPoint, { wsEndpoint: convertHttpToWebSocket(rpcEndPoint), commitment: "processed" });
+    const payerKeypair = Keypair.fromSecretKey(base58.decode(payer))
+
+    console.log('Your Pub Key => ', payerKeypair.publicKey.toString())
+
+    function sendRequest(ws: WebSocket) {
+        const request = {
+            jsonrpc: "2.0",
+            id: 420,
+            method: "transactionSubscribe",
+            params: [
+                {
+                    failed: false,
+                    accountInclude: ["6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"]
+                },
+                {
+                    commitment: "confirmed",
+                    encoding: "jsonParsed",
+                    transactionDetails: "full",
+                    maxSupportedTransactionVersion: 0
+                }
+            ]
+        };
+        ws.send(JSON.stringify(request));
+    }
+
+    ws.on('open', function open() {
+        console.log('WebSocket is open');
+        sendRequest(ws);  // Send a request once the WebSocket is open
+    });
+
+    ws.on('message', async function incoming(data) {
+        const messageStr = data.toString('utf8');
+        try {
+            const messageObj = JSON.parse(messageStr);
+
+            const result = messageObj.params.result;
+            const logs = result.transaction.meta.logMessages;
+            const signature = result.signature; // Extract the signature
+            const accountKeys = result.transaction.transaction.message.accountKeys.map((ak: { pubkey: any; }) => ak.pubkey);
+
+            if (logs && logs.some((log: string | string[]) => log.includes('Program log: Instruction: InitializeMint2'))) {
+                const dev = accountKeys[0]
+                const mint = accountKeys[1]
+
+                console.log("New signature => ", `https://solscan.io/tx/${signature}`, await formatDate());
+                if (isDevMode) {
+                    console.log("Dev wallet => ", `https://solscan.io/address/${dev}`);
+                }
+                if (isDevMode && dev !== devAddr) return;
+
+                if (isTickerMode) {
+                    if (!tokenTicker) return console.log("Token Ticker is not defiend!");
+                    const tokenInfo = await getTokenMetadata(mint.toString(), connection);
+                    if (!tokenInfo) return;
+                    const isTarget = tokenInfo.symbol.toUpperCase().includes(tokenTicker.toUpperCase())
+                    if (!isTarget) return
+                    console.log(`Found $${tokenInfo.symbol} token.`)
+                }
+
+                console.log('New token => ', `https://solscan.io/token/${mint.toString()}`)
+                ws.close();
+                const mintPub = new PublicKey(mint);
+                const sig = await buyToken(mintPub, connection, payerKeypair, solIn, 1);
+                console.log('Buy Transaction => ', `https://solscan.io/tx/${sig}`)
+                if (!sig) {
+                    ws.on('open', function open() {
+                        console.log('WebSocket is open');
+                        sendRequest(ws);  // Send a request once the WebSocket is open
+                    });
+                } else {
+                    console.log('🚀 Buy Success!!!');
+                    console.log('Try to sell on pumpfun: ', `https://pump.fun/${mint.toString()}`)
+                }
+
+            }
+        } catch (e) {
+
+        }
+    });
+
+}
+
+const runBot = () => {
+    if (isGeyser) {
+        console.log('--------------- Geyser mode selected! ---------------\n');
+        withGaser(rpc!, payer!, Number(buyamount!), devwallet!);
+    } else {
+        console.log("--------------- Common Mode selected! ---------------\n");
+        init(rpc!, payer!, Number(buyamount!), devwallet!)
+    }
+}
+
+const getTokenMetadata = async (mintAddress: string, connection: Connection) => {
+    try {
+        const metaplex = Metaplex.make(connection);
+        const mintPublicKey = new PublicKey(mintAddress);
+        const nft = await metaplex.nfts().findByMint({ mintAddress: mintPublicKey });
+        return nft;  // Returns the token's ticker/symbol
+    } catch (error) {
+        //   console.error("Error fetching token metadata:", error);
+        return false
+    }
+};
+
+runBot()
---
+++ src/pumputils/utils/buyToken.ts
@@ -0,0 +1,228 @@
+import * as token from "@solana/spl-token";
+import * as web3 from "@solana/web3.js";
+import getBondingCurvePDA from "./getBondingCurvePDA";
+import tokenDataFromBondingCurveTokenAccBuffer from "./tokenDataFromBondingCurveTokenAccBuffer";
+import getBuyPrice from "./getBuyPrice";
+import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
+import { BN } from "bn.js";
+import { PumpFun } from "../idl/pump-fun";
+import IDL from "../idl/pump-fun.json";
+import wait from "./wait";
+import getBondingCurveTokenAccountWithRetry from "./getBondingCurveTokenAccountWithRetry";
+import { SystemProgram, TransactionMessage } from "@solana/web3.js";
+import { executeJitoTx } from "../../utils/jito";
+
+const BOANDING_CURVE_ACC_RETRY_AMOUNT = 5;
+const BOANDING_CURVE_ACC_RETRY_DELAY = 50;
+
+
+interface Payload {
+  transaction: TransactionMessages;
+}
+
+interface TransactionMessages {
+  content: string;
+}
+
+async function buyToken(
+  mint: web3.PublicKey,
+  connection: web3.Connection,
+  keypair: web3.Keypair,
+  solAmount: number,
+  slippage: number,
+  priorityFee?: number
+) {
+  try {
+    // Load Pumpfun provider
+    const provider = new AnchorProvider(connection, new Wallet(keypair), {
+      commitment: "finalized",
+    });
+    const program = new Program<PumpFun>(IDL as PumpFun, provider);
+
+    // Create transaction
+    const transaction = new web3.Transaction();
+
+    const tokenAccounts = await connection.getTokenAccountsByOwner(
+      keypair.publicKey,
+      {
+        mint: mint,
+      }
+    );
+    console.log("🚀 ~ tokenAccounts:", tokenAccounts)
+
+    // Get/Create token account
+    const associatedUser = await token.getAssociatedTokenAddress(mint, keypair.publicKey, false);
+
+    if (tokenAccounts.value.length !== 0) {
+      // await token.getAccount(connection, associatedUser, "finalized");
+    } else {
+      transaction.add(
+        token.createAssociatedTokenAccountInstruction(keypair.publicKey, associatedUser, keypair.publicKey, mint)
+      );
+    }
+    // console.log("Account created/loaded");
+
+    // Buy token on Pump.fun
+    // console.log("Buying");
+    const programId = new web3.PublicKey('6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P');
+
+    const bondingCurve = getBondingCurvePDA(mint, programId);
+    const associatedBondingCurve = await token.getAssociatedTokenAddress(mint, bondingCurve, true);
+    // let BondingCurveTokenAccount: web3.AccountInfo<Buffer> | null = null;
+
+    const bondingCurveTokenAccount = await getBondingCurveTokenAccountWithRetry(
+      connection,
+      bondingCurve,
+      BOANDING_CURVE_ACC_RETRY_AMOUNT,
+      BOANDING_CURVE_ACC_RETRY_DELAY
+    );
+
+    if (bondingCurveTokenAccount === null) {
+      throw new Error("Bonding curve account not found");
+    }
+    const tokenData = tokenDataFromBondingCurveTokenAccBuffer(bondingCurveTokenAccount!.data);
+    if (tokenData.complete) {
+      throw new Error("Bonding curve already completed");
+    }
+    console.log('transfer sol amount => ', solAmount);
+    const SLIPAGE_POINTS = BigInt(slippage * 100);
+    const solAmountLamp = BigInt(solAmount * web3.LAMPORTS_PER_SOL);
+    console.log("🚀 ~ solAmountLamp:", solAmountLamp)
+    const buyAmountToken = getBuyPrice(solAmountLamp, tokenData);
+    const buyAmountSolWithSlippage = solAmountLamp + (solAmountLamp * SLIPAGE_POINTS) / 10000n;
+
+    const FEE_RECEIPT = new web3.PublicKey("CebN5WGQ4jvEPvsVU4EoHEpgzq1VV7AbicfhtW4xC9iM");
+
+    // request a specific compute unit budget
+    const modifyComputeUnits = web3.ComputeBudgetProgram.setComputeUnitLimit({
+      units: 100000,
+    });
+
+    // set the desired priority fee
+    const addPriorityFee = web3.ComputeBudgetProgram.setComputeUnitPrice({
+      microLamports: typeof priorityFee === "number" ? priorityFee * 1000000000 : 0.0001 * 1000000000,
+      // microLamports: 0.005 * 1000000000,
+    });
+
+    const latestBlockhash = await connection.getLatestBlockhash()
+
+    transaction
+      .add(modifyComputeUnits)
+      .add(addPriorityFee)
+      .add(
+        await program.methods
+          .buy(new BN(buyAmountToken.toString()), new BN(buyAmountSolWithSlippage.toString()))
+          .accounts({
+            feeRecipient: FEE_RECEIPT,
+            mint: mint,
+            associatedBondingCurve: associatedBondingCurve,
+            associatedUser: associatedUser,
+            user: keypair.publicKey,
+          })
+          .transaction()
+      );
+
+    transaction.feePayer = keypair.publicKey;
+    transaction.recentBlockhash = latestBlockhash.blockhash;
+
+    console.log(await connection.simulateTransaction(transaction))
+
+    const isNext = process.env.IS_NEXT === 'true';
+    const isJito = process.env.IS_JITO === 'true';
+
+    if (isNext) {
+      const next_block_addrs = [
+        'NEXTbLoCkB51HpLBLojQfpyVAMorm3zzKg7w9NFdqid',
+        'NeXTBLoCKs9F1y5PJS9CKrFNNLU1keHW71rfh7KgA1X',
+        'NexTBLockJYZ7QD7p2byrUa6df8ndV2WSd8GkbWqfbb',
+        'neXtBLock1LeC67jYd1QdAa32kbVeubsfPNTJC1V5At',
+        'nEXTBLockYgngeRmRrjDV31mGSekVPqZoMGhQEZtPVG',
+        'nextBLoCkPMgmG8ZgJtABeScP35qLa2AMCNKntAP7Xc',
+        'NextbLoCkVtMGcV47JzewQdvBpLqT9TxQFozQkN98pE',
+        'NexTbLoCkWykbLuB1NkjXgFWkX9oAtcoagQegygXXA2'
+      ]
+
+      for (let i = 0; i < next_block_addrs.length; i++) {
+        const next_block_addr = next_block_addrs[i];
+        const next_block_api = process.env.NEXT_BLOCK_API;
+
+        if (!next_block_addr) return console.log("Nextblock wallet is not provided");
+        if (!next_block_api) return console.log("Nextblock block api is not provided");
+
+        // NextBlock Instruction
+        const recipientPublicKey = new web3.PublicKey(next_block_addr);
+        const transferInstruction = SystemProgram.transfer({
+          fromPubkey: keypair.publicKey,
+          toPubkey: recipientPublicKey,
+          lamports: process.env.NEXT_BLOCK_FEE ? Number(process.env.NEXT_BLOCK_FEE) * web3.LAMPORTS_PER_SOL : 1000000
+        });
+
+        transaction.add(transferInstruction);
+
+        transaction.sign(keypair)
+
+        console.log("Buying token")
+
+        const tx64Str = transaction.serialize().toString('base64');
+        const payload: Payload = {
+          transaction: {
+            content: tx64Str
+          }
+        };
+
+        try {
+          console.log("Trying transaction to confirm using nextblock")
+          const response = await fetch('https://fra.nextblock.io/api/v2/submit', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'authorization': next_block_api // Insert your authorization token here
+            },
+            body: JSON.stringify(payload)
+          });
+
+          const responseData = await response.json();
+          console.log("responseData", responseData);
+
+          if (response.ok) {
+            console.log("Sent transaction with signature", `https://solscan.io/tx/${responseData.signature?.toString()}`);
+            break;
+          } else {
+            console.error("Failed to send transaction:", response.status, responseData);
+            continue;
+          }
+        } catch (error) {
+          console.error("Error sending transaction:", error);
+          continue;
+        }
+      }
+    } else if (isJito) {
+
+      const messageV0 = new TransactionMessage({
+        payerKey: keypair.publicKey,
+        recentBlockhash: latestBlockhash.blockhash,
+        instructions: transaction.instructions,
+      }).compileToV0Message()
+
+      const versionedTx = new web3.VersionedTransaction(messageV0);
+      versionedTx.sign([keypair]);
+
+      const sig = await executeJitoTx([versionedTx], keypair, 'confirmed');
+      return sig
+    } else {
+      const txSig = await connection.sendTransaction(transaction, [keypair]);
+      const confirmSig = await connection.confirmTransaction(txSig, 'confirmed');
+
+      console.log('confirm sig => ', confirmSig.value.err);
+
+      console.log(`Sending transaction for buying ${mint.toString()}, ${new Date().toISOString()}`);
+      const sig = await web3.sendAndConfirmTransaction(connection, transaction, [keypair], { skipPreflight: true, commitment: "confirmed", preflightCommitment: 'confirmed' });
+      return sig
+    }
+
+  } catch (error) {
+    console.error(error);
+    return false
+  }
+}
+export default buyToken;
